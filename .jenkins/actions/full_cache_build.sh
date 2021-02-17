b832d0804023ea4b7d37453a915a46bca48b4647
tobias <twicki12@gmail.com>
2021-02-17 04:02:29 +0100

    set root dir

--- .jenkins/actions/full_cache_build.sh
+++ .jenkins/actions/full_cache_build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e -x
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+ROOT_DIR="$(dirname "$(dirname "$SCRIPTPATH")")"
 
 git clone git@github.com:VulcanClimateModeling/daint_venv.git
 cd daint_venv
