8341b8bd5455c364a0d8c73838c2bc1d4ae4af46
Rhea George <rheag@vulcan.com>
2021-08-05 16:19:51 -0700

    omg are you serious right now, we wanted to try all BaseSLicing

--- tests/translate/translate_haloupdate.py
+++ tests/translate/translate_haloupdate.py
@@ -9,7 +9,7 @@ from fv3core.utils import gt4py_utils as utils
 logger = logging.getLogger("fv3ser")
 
 
-class TranslateHaloUpdate(ParallelTranslate):
+class TranslateHaloUpdate(ParallelTranslateBaseSlicing):
 
     inputs = {
         "array": {
